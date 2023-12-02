4569281686520a71feef969df3d34d13d964e355
wambuastephen <wambuastephen97@gmail.com>
2023-12-03 00:02:50 +0300

    ...

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
-language that combines remarkable power with very clear syntax"
-print(str[39:72] + str[106:112] + str[:6])
+ language that combines remarkable power with very clear syntax"
+str = str[39:-63] + str[106:-17] + str[:6]
+print(str)
