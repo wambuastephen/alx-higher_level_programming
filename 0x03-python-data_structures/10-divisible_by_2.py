9c6fc859b711d806db6df3ab8143e17b46d3bfc3
wambuastephen <wambuastephen97@gmail.com>
2023-12-02 22:43:50 +0300

    11-delete_at.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    result = []        
+    result = []
     for num in my_list:
-        result.append(num % 2 == 0)                        
+        result.append(num % 2 == 0)
     return result
